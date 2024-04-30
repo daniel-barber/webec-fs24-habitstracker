5e093835cd277841f8208a2c0cd48ac29785f86b
daniel.barber <daniel.barber@students.fhnw.ch>
2024-04-30 16:14:28 +0200

    add user class

---
+++ src/main/java/ch/fhnw/webec/exercise/model/User.java
@@ -0,0 +1,12 @@
+package ch.fhnw.webec.exercise.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.springframework.security.core.userdetails.UserDetails;
+
+//@Entity
+//@Table(name="User")
+//public class User implements UserDetails {
+//
+//
+//}
