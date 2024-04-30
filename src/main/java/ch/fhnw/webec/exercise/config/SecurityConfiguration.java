cd25303dd0c6edf9551d134037c8b6a130f4e43a
daniel.barber <daniel.barber@students.fhnw.ch>
2024-04-30 16:18:47 +0200

    add Permit access to all static resources

--- src/main/java/ch/fhnw/webec/exercise/config/SecurityConfiguration.java
+++ src/main/java/ch/fhnw/webec/exercise/config/SecurityConfiguration.java
@@ -1,10 +1,13 @@
 package ch.fhnw.webec.exercise.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return null;
+        return http.authorizeHttpRequests(authz -> authz
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+        ).build();
     }
 }
