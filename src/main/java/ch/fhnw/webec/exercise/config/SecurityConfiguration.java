320f3c9a52c94a08455a87bd0ea6f24e871e2913
daniel.barber <daniel.barber@students.fhnw.ch>
2024-04-30 16:14:44 +0200

    add SecurityConfiguration class

---
+++ src/main/java/ch/fhnw/webec/exercise/config/SecurityConfiguration.java
@@ -0,0 +1,10 @@
+package ch.fhnw.webec.exercise.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfiguration {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return null;
+    }
+}
