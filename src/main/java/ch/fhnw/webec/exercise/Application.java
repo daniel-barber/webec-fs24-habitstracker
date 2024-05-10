a469da2ed9bdecc6f132be3d86f9f7df4453ffee
daniel.barber <daniel.barber@students.fhnw.ch>
2024-05-10 17:16:07 +0200

    rollback

--- src/main/java/ch/fhnw/webec/exercise/Application.java
+++ src/main/java/ch/fhnw/webec/exercise/Application.java
@@ -2,7 +2,7 @@ package ch.fhnw.webec.exercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//back
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
