9ebd8219081a071358039a9628df29bbca10461a
daniel.barber <daniel.barber@students.fhnw.ch>
2024-05-24 18:30:03 +0200

    fix timezone issue

--- src/main/java/ch/fhnw/webec/exercise/Application.java
+++ src/main/java/ch/fhnw/webec/exercise/Application.java
@@ -2,9 +2,19 @@ package ch.fhnw.webec.exercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import java.util.TimeZone;
+
 //back
 @SpringBootApplication
 public class Application {
+
+    // Help from GPT to fix timezone issue
+    @EventListener
+    void contextRefreshedEvent(ContextRefreshedEvent event) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Zurich"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
